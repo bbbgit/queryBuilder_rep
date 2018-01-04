c1af61595799f872e8d790750288e98d182562e3
bb <bb@bb-PC>
2018-01-04 23:03:51 +0100

    vracen update za stare uslove na prvobitni query "dfin is null"

--- query_builder_git/src/send/constructQuery.java
+++ query_builder_git/src/send/constructQuery.java
@@ -18,8 +18,8 @@ public class constructQuery {
 			 	//Cistimo array za svaki sllucaj
 			 	Arrays.fill(sadrzaj, null);
 			 	
-			  //if(ExpireOld==true)	{sadrzaj[0] = "update bkcond set dfin=SYSDATE-1 where dfin is null and ope in ('024','025') and nat in ('COMVER','MACVER','MAVDEP','MICVER','MIVDEP') and cli='"+root+"';";}
-				if(ExpireOld==true)	{sadrzaj[0] = "update bkcond set dfin=SYSDATE-1 where ope in ('024','025') and nat in ('COMVER','MACVER','MAVDEP','MICVER','MIVDEP') and cli='"+root+"';";}
+			    if(ExpireOld==true)	{sadrzaj[0] = "update bkcond set dfin=SYSDATE-1 where dfin is null and ope in ('024','025') and nat in ('COMVER','MACVER','MAVDEP','MICVER','MIVDEP') and cli='"+root+"';";}
+				//if(ExpireOld==true)	{sadrzaj[0] = "update bkcond set dfin=SYSDATE-1 where ope in ('024','025') and nat in ('COMVER','MACVER','MAVDEP','MICVER','MIVDEP') and cli='"+root+"';";}
 				else {sadrzaj[0] = "";};
 				sadrzaj[1] =  "Insert into bkcond (AGE,OPE,DEV,BAN,PRO,CLI,CPT,SUF,NAT,VAL,DEVR,FOR1,VARP1,VARM1,FOR2,VARP2,VARM2,FOR3,VARP3,VARM3,FOR4,VARP4,VARM4,FOR5,VARP5,VARM5,DOU,DMO,CUTI,DREF,CLEUNI,DFIN,CPROD,CPACK,REMISE,POIDS,TYPREMISE) values ('99000','024','***','N','   ','"+root+"       ','           ','  ','COMVER',0,'   ',null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,SYSDATE,null,'"+(System.getProperty("user.name")).toUpperCase()+"  ',SYSDATE,'99000      ***024***N',null,'***','      ','N',201121,' ');"; 
 				sadrzaj[2] =  "Insert into bkcond (AGE,OPE,DEV,BAN,PRO,CLI,CPT,SUF,NAT,VAL,DEVR,FOR1,VARP1,VARM1,FOR2,VARP2,VARM2,FOR3,VARP3,VARM3,FOR4,VARP4,VARM4,FOR5,VARP5,VARM5,DOU,DMO,CUTI,DREF,CLEUNI,DFIN,CPROD,CPACK,REMISE,POIDS,TYPREMISE) values ('99000','024','***','N','   ','"+root+"       ','           ','  ','MACVER',0,'941',null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,SYSDATE,null,'"+(System.getProperty("user.name")).toUpperCase()+"  ',SYSDATE,'99000      ***024***N',null,'***','      ','N',201121,' ');";
@@ -45,8 +45,8 @@ public class constructQuery {
         	 	//Cistimo array za svaki sllucaj
 			 	Arrays.fill(sadrzaj, null);
 
-			  //if(ExpireOld==true)	{sadrzaj[0] = "update bkcond set dfin=SYSDATE-1 where dfin is null and ope in ('024','025') and nat in ('COMVER','MACVER','MAVDEP','MICVER','MIVDEP') and cli='"+root+"';";}
-				if(ExpireOld==true)	{sadrzaj[0] = "update bkcond set dfin=SYSDATE-1 where ope in ('024','025') and nat in ('COMVER','MACVER','MAVDEP','MICVER','MIVDEP') and cli='"+root+"';";}
+			    if(ExpireOld==true)	{sadrzaj[0] = "update bkcond set dfin=SYSDATE-1 where dfin is null and ope in ('024','025') and nat in ('COMVER','MACVER','MAVDEP','MICVER','MIVDEP') and cli='"+root+"';";}
+				//if(ExpireOld==true)	{sadrzaj[0] = "update bkcond set dfin=SYSDATE-1 where ope in ('024','025') and nat in ('COMVER','MACVER','MAVDEP','MICVER','MIVDEP') and cli='"+root+"';";}
 				else {sadrzaj[0] = "";};
 				sadrzaj[1] =  "Insert into bkcond (AGE,OPE,DEV,BAN,PRO,CLI,CPT,SUF,NAT,VAL,DEVR,FOR1,VARP1,VARM1,FOR2,VARP2,VARM2,FOR3,VARP3,VARM3,FOR4,VARP4,VARM4,FOR5,VARP5,VARM5,DOU,DMO,CUTI,DREF,CLEUNI,DFIN,CPROD,CPACK,REMISE,POIDS,TYPREMISE) values ('99000','024','***','N','   ','"+root+"       ','           ','  ','COMVER',0,'   ',null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,SYSDATE,null,'"+(System.getProperty("user.name")).toUpperCase()+"  ',SYSDATE,'99000      ***024***N',SYSDATE+"+DaysActive+",'***','      ','N',201121,' ');"; 
 				sadrzaj[2] =  "Insert into bkcond (AGE,OPE,DEV,BAN,PRO,CLI,CPT,SUF,NAT,VAL,DEVR,FOR1,VARP1,VARM1,FOR2,VARP2,VARM2,FOR3,VARP3,VARM3,FOR4,VARP4,VARM4,FOR5,VARP5,VARM5,DOU,DMO,CUTI,DREF,CLEUNI,DFIN,CPROD,CPACK,REMISE,POIDS,TYPREMISE) values ('99000','024','***','N','   ','"+root+"       ','           ','  ','MACVER',0,'941',null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,SYSDATE,null,'"+(System.getProperty("user.name")).toUpperCase()+"  ',SYSDATE,'99000      ***024***N',SYSDATE+"+DaysActive+",'***','      ','N',201121,' ');";
@@ -72,8 +72,8 @@ public class constructQuery {
         	 	//Cistimo array za svaki sllucaj
 			 	Arrays.fill(sadrzaj, null);
         		
-			  //if(ExpireOld==true)	{sadrzaj[0] = "update bkcond set dfin=SYSDATE-1 where dfin is null and ope in ('024','025') and nat in ('COMVER','MACVER','MAVDEP','MICVER','MIVDEP') and cli='"+root+"';";}
-        		if(ExpireOld==true)	{sadrzaj[0] = "update bkcond set dfin=SYSDATE-1 where ope in ('024','025') and nat in ('COMVER','MACVER','MAVDEP','MICVER','MIVDEP') and cli='"+root+"';";}
+			    if(ExpireOld==true)	{sadrzaj[0] = "update bkcond set dfin=SYSDATE-1 where dfin is null and ope in ('024','025') and nat in ('COMVER','MACVER','MAVDEP','MICVER','MIVDEP') and cli='"+root+"';";}
+        		//if(ExpireOld==true)	{sadrzaj[0] = "update bkcond set dfin=SYSDATE-1 where ope in ('024','025') and nat in ('COMVER','MACVER','MAVDEP','MICVER','MIVDEP') and cli='"+root+"';";}
 				else {sadrzaj[0] = "";};
 				sadrzaj[1] =  "Insert into bkcond (AGE,OPE,DEV,BAN,PRO,CLI,CPT,SUF,NAT,VAL,DEVR,FOR1,VARP1,VARM1,FOR2,VARP2,VARM2,FOR3,VARP3,VARM3,FOR4,VARP4,VARM4,FOR5,VARP5,VARM5,DOU,DMO,CUTI,DREF,CLEUNI,DFIN,CPROD,CPACK,REMISE,POIDS,TYPREMISE) values ('99000','024','***','N','   ','"+root+"       ','           ','  ','COMVER',"+Percent+",'   ',null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,SYSDATE,null,'"+(System.getProperty("user.name")).toUpperCase()+"  ',SYSDATE,'99000      ***024***N',null,'***','      ','N',301121,' ');"; 
 				sadrzaj[2] =  "Insert into bkcond (AGE,OPE,DEV,BAN,PRO,CLI,CPT,SUF,NAT,VAL,DEVR,FOR1,VARP1,VARM1,FOR2,VARP2,VARM2,FOR3,VARP3,VARM3,FOR4,VARP4,VARM4,FOR5,VARP5,VARM5,DOU,DMO,CUTI,DREF,CLEUNI,DFIN,CPROD,CPACK,REMISE,POIDS,TYPREMISE) values ('99000','024','***','N','   ','"+root+"       ','           ','  ','MACVER',"+Max+",'941',null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,SYSDATE,null,'"+(System.getProperty("user.name")).toUpperCase()+"  ',SYSDATE,'99000      ***024***N',null,'***','      ','N',301121,' ');";
@@ -97,8 +97,8 @@ public class constructQuery {
         	//Cistimo array za svaki sllucaj
 			 	Arrays.fill(sadrzaj, null);
 
-			  //if(ExpireOld==true)	{sadrzaj[0] = "update bkcond set dfin=SYSDATE-1 where dfin is null and ope in ('024','025') and nat in ('COMVER','MACVER','MAVDEP','MICVER','MIVDEP') and cli='"+root+"';";}
-				if(ExpireOld==true)	{sadrzaj[0] = "update bkcond set dfin=SYSDATE-1 where ope in ('024','025') and nat in ('COMVER','MACVER','MAVDEP','MICVER','MIVDEP') and cli='"+root+"';";}
+			    if(ExpireOld==true)	{sadrzaj[0] = "update bkcond set dfin=SYSDATE-1 where dfin is null and ope in ('024','025') and nat in ('COMVER','MACVER','MAVDEP','MICVER','MIVDEP') and cli='"+root+"';";}
+				//if(ExpireOld==true)	{sadrzaj[0] = "update bkcond set dfin=SYSDATE-1 where ope in ('024','025') and nat in ('COMVER','MACVER','MAVDEP','MICVER','MIVDEP') and cli='"+root+"';";}
 				else {sadrzaj[0] = "";};
 				sadrzaj[1] =  "Insert into bkcond (AGE,OPE,DEV,BAN,PRO,CLI,CPT,SUF,NAT,VAL,DEVR,FOR1,VARP1,VARM1,FOR2,VARP2,VARM2,FOR3,VARP3,VARM3,FOR4,VARP4,VARM4,FOR5,VARP5,VARM5,DOU,DMO,CUTI,DREF,CLEUNI,DFIN,CPROD,CPACK,REMISE,POIDS,TYPREMISE) values ('99000','024','***','N','   ','"+root+"       ','           ','  ','COMVER',"+Percent+",'   ',null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,SYSDATE,null,'"+(System.getProperty("user.name")).toUpperCase()+"  ',SYSDATE,'99000      ***024***N',SYSDATE+"+DaysActive+",'***','      ','N',301121,' ');"; 
 				sadrzaj[2] =  "Insert into bkcond (AGE,OPE,DEV,BAN,PRO,CLI,CPT,SUF,NAT,VAL,DEVR,FOR1,VARP1,VARM1,FOR2,VARP2,VARM2,FOR3,VARP3,VARM3,FOR4,VARP4,VARM4,FOR5,VARP5,VARM5,DOU,DMO,CUTI,DREF,CLEUNI,DFIN,CPROD,CPACK,REMISE,POIDS,TYPREMISE) values ('99000','024','***','N','   ','"+root+"       ','           ','  ','MACVER',"+Max+",'941',null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,SYSDATE,null,'"+(System.getProperty("user.name")).toUpperCase()+"  ',SYSDATE,'99000      ***024***N',SYSDATE+"+DaysActive+",'***','      ','N',301121,' ');";
